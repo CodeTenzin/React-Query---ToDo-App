66fdde081a0bcc5650d83dcb19fae436f44686b5
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-01-31 18:03:39 -0500

    Fetch data with react query.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TodoList />
+    </>
+  );
 }
 
 export default App;

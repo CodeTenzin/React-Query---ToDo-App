9f95148fa1cb3f0823a065d3ee62118293d48b1e
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-01-31 23:22:35 -0500

    Build fail on infinite queries.

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,26 @@
 import { useState } from "react";
 import usePosts from "./hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
   const pageSize = 10;
-  const [page, setPage] = useState(1);
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
-  // const { data: posts, error, isLoading } = usePosts(userId);
-
-  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
   return (
     <>
       {error && <p>{error.message}</p>}
       {isLoading && <p>Loading</p>}
 
-      <select
-        onChange={(event) => setUserId(parseInt(event.target.value))}
-        className="form-select mb-3"
-      >
-        <option value=""></option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-      </select>
-
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
     </>

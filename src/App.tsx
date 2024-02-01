e550b4c2b135688a889d353a32e4bfe127d664f5
Tenzin Wangyal <tenz_wang333@yahoo.com>
2024-01-31 23:55:06 -0500

    Mutating data recheck TodoForm.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import TodoList from "./react-query/TodoList";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 
 function App() {
   return (
     <>
       {/* <TodoList /> */}
-      <PostList />
+      {/* <PostList /> */}
+      <TodoForm />
     </>
   );
 }
